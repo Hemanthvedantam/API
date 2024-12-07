9808302ad73a5f4380e1aeb7bfda8c0d5c6ab2e4
Hemanth Hemanth <154527901+Hemanthvedantam@users.noreply.github.com>
2024-12-07 10:26:19 +0530

    Delete OrderRepository.java

--- OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.klef.jfsd.exam.repository;
-
-import com.klef.jfsd.exam.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, String> {
-}
